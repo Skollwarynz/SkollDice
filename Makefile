36eabf3bf196647c721149822865d9d9dcf6e20d
Skollwarynz <Zante342@gmail.com>
2026-08-22 15:50:06 +0200

    Final version of beta

--- Makefile
+++ Makefile
@@ -1,16 +1,58 @@
+# Compilatore e flag
 CC = gcc
-CFLAGS = -Wall -Wextra -I. -ISkollDice/include -ISkollDice/src -Idependencies/lvgl -Idependencies/lvgl/include -Idependencies/lvgl/src/drivers/sdl $(shell pkg-config --cflags sdl2)
-LDFLAGS = $(shell pkg-config --libs sdl2) -lm
+CFLAGS = -Wall -Wextra -O3 \
+         -I./SkollDice \
+         -I./SkollDice/include \
+         -I./SkollDice/src \
+         -I./SkollDice/dependencies \
+         -I./SkollDice/dependencies/lvgl
 
-SRC = SkollDice/src/main.c \
-      SkollDice/src/random_dice.c \
-      dependencies/lvgl/src/drivers/sdl/lv_sdl_window.c \
-      dependencies/lvgl/src/drivers/sdl/lv_sdl_mouse.c \
-      dependencies/lvgl/src/drivers/sdl/lv_sdl_keyboard.c \
-      dependencies/lvgl/src/drivers/sdl/lv_sdl_mousewheel.c
+# Flag di SDL2
+SDL_CFLAGS := $(shell pkg-config --cflags sdl2)
+SDL_LDFLAGS := $(shell pkg-config --libs sdl2)
 
-all:
-	$(CC) $(CFLAGS) $(SRC) -Ldependencies/lvgl -llvgl $(LDFLAGS) -o SkollDice
+CFLAGS += $(SDL_CFLAGS)
+
+# File sorgente del tuo progetto
+SRC = SkollDice/src/main.c SkollDice/src/random_dice.c
+
+# Sorgenti core di LVGL (prendiamo solo le cartelle indispensabili per SDL2)
+LVGL_DIR = SkollDice/dependencies/lvgl
+LVGL_SRC = \
+    $(wildcard $(LVGL_DIR)/src/core/*.c) \
+    $(wildcard $(LVGL_DIR)/src/draw/*.c) \
+    $(wildcard $(LVGL_DIR)/src/draw/sw/*.c) \
+    $(wildcard $(LVGL_DIR)/src/draw/sw/blend/*.c) \
+    $(wildcard $(LVGL_DIR)/src/font/*.c) \
+    $(wildcard $(LVGL_DIR)/src/hal/*.c) \
+    $(wildcard $(LVGL_DIR)/src/โหd/*.c) \
+    $(wildcard $(LVGL_DIR)/src/indev/*.c) \
+    $(wildcard $(LVGL_DIR)/src/layouts/*.c) \
+    $(wildcard $(LVGL_DIR)/src/stdlib/*.c) \
+    $(wildcard $(LVGL_DIR)/src/misc/*.c) \
+    $(wildcard $(LVGL_DIR)/src/widgets/*.c) \
+    $(wildcard $(LVGL_DIR)/src/drivers/sdl/*.c) \
+    $(wildcard $(LVGL_DIR)/src/osal/*.c) \
+    $(wildcard $(LVGL_DIR)/src/*.c)
+
+# Tutti gli oggetti da compilare
+OBJ = $(SRC:.c=.o) $(LVGL_SRC:.c=.o)
+
+# Nome dell'eseguibile finale
+TARGET = SkollDice_AppImage
+
+all: $(TARGET)
+
+$(TARGET): $(OBJ)
+	@echo "Linking $(TARGET)..."
+	$(CC) $(OBJ) -o $(TARGET) $(SDL_LDFLAGS) -lm -lpthread
+
+%.o: %.c
+	@echo "Compiling $<..."
+	$(CC) $(CFLAGS) -c $< -o $@
 
 clean:
-	rm -f SkollDice
+	@echo "Pulizia dei file oggetto..."
+	rm -f $(OBJ) $(TARGET)
+
+.PHONY: all clean
---
+++ build_appimage/SkollDice/dependencies/lvgl/cmake_install.cmake
@@ -0,0 +1,162 @@
+# Install script for directory: /home/skollwarynz/DnD_truly_random_dice/SkollDice/dependencies/lvgl
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "0")
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+# Set path to fallback-tool for dependency-resolution.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE DIRECTORY FILES "/home/skollwarynz/DnD_truly_random_dice/SkollDice/dependencies/lvgl/include/lvgl")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/lvgl/config" TYPE FILE RENAME "lv_conf.h" FILES "/home/skollwarynz/DnD_truly_random_dice/lv_conf.h")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig" TYPE FILE FILES "/home/skollwarynz/DnD_truly_random_dice/build_appimage/SkollDice/dependencies/lvgl/lvgl.pc")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/lvgl" TYPE FILE FILES
+    "/home/skollwarynz/DnD_truly_random_dice/build_appimage/SkollDice/dependencies/lvgl/lvglConfig.cmake"
+    "/home/skollwarynz/DnD_truly_random_dice/build_appimage/SkollDice/dependencies/lvgl/lvglConfigVersion.cmake"
+    )
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "/home/skollwarynz/DnD_truly_random_dice/build_appimage/SkollDice/dependencies/lvgl/liblvgl.a")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/lvgl/lvglTargets.cmake")
+    file(DIFFERENT _cmake_export_file_changed FILES
+         "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/lvgl/lvglTargets.cmake"
+         "/home/skollwarynz/DnD_truly_random_dice/build_appimage/SkollDice/dependencies/lvgl/CMakeFiles/Export/dd984d6d4882ae81031482330785511f/lvglTargets.cmake")
+    if(_cmake_export_file_changed)
+      file(GLOB _cmake_old_config_files "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/lvgl/lvglTargets-*.cmake")
+      if(_cmake_old_config_files)
+        string(REPLACE ";" ", " _cmake_old_config_files_text "${_cmake_old_config_files}")
+        message(STATUS "Old export file \"$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/lvgl/lvglTargets.cmake\" will be replaced.  Removing files [${_cmake_old_config_files_text}].")
+        unset(_cmake_old_config_files_text)
+        file(REMOVE ${_cmake_old_config_files})
+      endif()
+      unset(_cmake_old_config_files)
+    endif()
+    unset(_cmake_export_file_changed)
+  endif()
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/lvgl" TYPE FILE FILES "/home/skollwarynz/DnD_truly_random_dice/build_appimage/SkollDice/dependencies/lvgl/CMakeFiles/Export/dd984d6d4882ae81031482330785511f/lvglTargets.cmake")
+  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^()$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/lvgl" TYPE FILE FILES "/home/skollwarynz/DnD_truly_random_dice/build_appimage/SkollDice/dependencies/lvgl/CMakeFiles/Export/dd984d6d4882ae81031482330785511f/lvglTargets-noconfig.cmake")
+  endif()
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "/home/skollwarynz/DnD_truly_random_dice/build_appimage/SkollDice/dependencies/lvgl/lib/liblvgl_thorvg.a")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/lvgl" TYPE DIRECTORY FILES "/home/skollwarynz/DnD_truly_random_dice/SkollDice/dependencies/lvgl/demos" FILES_MATCHING REGEX "/[^/]*\\.h$")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "/home/skollwarynz/DnD_truly_random_dice/build_appimage/SkollDice/dependencies/lvgl/liblvgl_demos.a")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/lvgl/lvglDemosTargets.cmake")
+    file(DIFFERENT _cmake_export_file_changed FILES
+         "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/lvgl/lvglDemosTargets.cmake"
+         "/home/skollwarynz/DnD_truly_random_dice/build_appimage/SkollDice/dependencies/lvgl/CMakeFiles/Export/dd984d6d4882ae81031482330785511f/lvglDemosTargets.cmake")
+    if(_cmake_export_file_changed)
+      file(GLOB _cmake_old_config_files "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/lvgl/lvglDemosTargets-*.cmake")
+      if(_cmake_old_config_files)
+        string(REPLACE ";" ", " _cmake_old_config_files_text "${_cmake_old_config_files}")
+        message(STATUS "Old export file \"$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/lvgl/lvglDemosTargets.cmake\" will be replaced.  Removing files [${_cmake_old_config_files_text}].")
+        unset(_cmake_old_config_files_text)
+        file(REMOVE ${_cmake_old_config_files})
+      endif()
+      unset(_cmake_old_config_files)
+    endif()
+    unset(_cmake_export_file_changed)
+  endif()
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/lvgl" TYPE FILE FILES "/home/skollwarynz/DnD_truly_random_dice/build_appimage/SkollDice/dependencies/lvgl/CMakeFiles/Export/dd984d6d4882ae81031482330785511f/lvglDemosTargets.cmake")
+  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^()$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/lvgl" TYPE FILE FILES "/home/skollwarynz/DnD_truly_random_dice/build_appimage/SkollDice/dependencies/lvgl/CMakeFiles/Export/dd984d6d4882ae81031482330785511f/lvglDemosTargets-noconfig.cmake")
+  endif()
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/lvgl" TYPE DIRECTORY FILES "/home/skollwarynz/DnD_truly_random_dice/SkollDice/dependencies/lvgl/examples" FILES_MATCHING REGEX "/[^/]*\\.h$")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "/home/skollwarynz/DnD_truly_random_dice/build_appimage/SkollDice/dependencies/lvgl/liblvgl_examples.a")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/lvgl/lvglExamplesTargets.cmake")
+    file(DIFFERENT _cmake_export_file_changed FILES
+         "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/lvgl/lvglExamplesTargets.cmake"
+         "/home/skollwarynz/DnD_truly_random_dice/build_appimage/SkollDice/dependencies/lvgl/CMakeFiles/Export/dd984d6d4882ae81031482330785511f/lvglExamplesTargets.cmake")
+    if(_cmake_export_file_changed)
+      file(GLOB _cmake_old_config_files "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/lvgl/lvglExamplesTargets-*.cmake")
+      if(_cmake_old_config_files)
+        string(REPLACE ";" ", " _cmake_old_config_files_text "${_cmake_old_config_files}")
+        message(STATUS "Old export file \"$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/lvgl/lvglExamplesTargets.cmake\" will be replaced.  Removing files [${_cmake_old_config_files_text}].")
+        unset(_cmake_old_config_files_text)
+        file(REMOVE ${_cmake_old_config_files})
+      endif()
+      unset(_cmake_old_config_files)
+    endif()
+    unset(_cmake_export_file_changed)
+  endif()
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/lvgl" TYPE FILE FILES "/home/skollwarynz/DnD_truly_random_dice/build_appimage/SkollDice/dependencies/lvgl/CMakeFiles/Export/dd984d6d4882ae81031482330785511f/lvglExamplesTargets.cmake")
+  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^()$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/lvgl" TYPE FILE FILES "/home/skollwarynz/DnD_truly_random_dice/build_appimage/SkollDice/dependencies/lvgl/CMakeFiles/Export/dd984d6d4882ae81031482330785511f/lvglExamplesTargets-noconfig.cmake")
+  endif()
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+if(CMAKE_INSTALL_LOCAL_ONLY)
+  file(WRITE "/home/skollwarynz/DnD_truly_random_dice/build_appimage/SkollDice/dependencies/lvgl/install_local_manifest.txt"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
+endif()
